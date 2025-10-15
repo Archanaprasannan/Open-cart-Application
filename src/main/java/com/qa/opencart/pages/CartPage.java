4890cd0dd4f088ecebddd69ef15879f443b0bcf0
Archana Prasannan <archanaprasannan2014@gmail.com>
2025-10-16 01:59:37 +0530

    made changes

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -1,9 +1,12 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class CartPage {
 	int i = 90;
+	private By name = By.id("name");
 
-	public void test() {
-		System.out.println("testing method");
+	public void newTest() {
+		System.out.println("testing method"+name);
 	}
 }
