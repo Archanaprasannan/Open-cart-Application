0f38659603e5c456b014ca2d6ad0693b3d399224
Archana Prasannan <archanaprasannan2014@gmail.com>
2025-10-16 22:12:08 +0530

    new Features 1 and 2 created

---
+++ src/main/java/com/qa/opencart/pages/DemoPage.java
@@ -0,0 +1,15 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.By;
+
+public class DemoPage {
+	By firtsname = By.id("first-name");
+
+	public void feature1() {
+		System.out.println("Feature 1 released");
+	}
+
+	public void feature2() {
+		System.out.println("Feature 2 released");
+	}
+}
